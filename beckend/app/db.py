04e0bd5b16c7fe993657c052406314276ddd7d45
kapilbhatt11 <kapil.bhatt111@gmail.com>
2026-07-28 14:54:37 +0530

    fix: disable statements cache in database for pgbouncer compatibility

--- beckend/app/db.py
+++ beckend/app/db.py
@@ -6,6 +6,6 @@ from databases import Database
 # Use environment variable of the hosted database in production, fallback to local URL
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:12345@localhost:5432/meripdf")
 
-database = Database(DATABASE_URL)
+database = Database(DATABASE_URL, min_size=1, max_size=20, statement_cache_size=0)
 metadata = MetaData()
 
